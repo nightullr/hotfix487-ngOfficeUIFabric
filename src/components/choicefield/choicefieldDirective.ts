7f7ab2d0a6d162994051df3e3d8d1e46ba3bbc6a
Lauren Woodbridge <laurenxamelia@gmail.com>
2017-08-19 16:35:15 -0400

    Added functionality for radio button vs checkbox behavior

--- src/components/choicefield/choicefieldDirective.ts
+++ src/components/choicefield/choicefieldDirective.ts
@@ -126,10 +126,23 @@ export class ChoicefieldOptionDirective implements angular.IDirective {
           'Supported options are listed here: ' +
           'https://github.com/ngOfficeUIFabric/ng-officeuifabric/blob/master/src/components/choicefield/choicefieldTypeEnum.ts');
       }
+      let modelValue: any = choicefieldGroupController.getViewValue();
+      if (newValue === 'radio' && modelValue && modelValue.constructor === Array) {
+        choicefieldGroupController.setViewValue(modelValue[0], null);
+      }
+      if (newValue === 'checkbox' && modelValue && modelValue.constructor === String) {
+        choicefieldGroupController.setViewValue([modelValue], null);
+      }
     });
     if (choicefieldGroupController != null) {
       let render: () => void = (): void => {
-        let checked: boolean = (choicefieldGroupController.getViewValue() === attrs.value);
+        let checked: boolean;
+        if (attrs.uifType == 'checkbox') {
+          checked = (choicefieldGroupController.getViewValue() && choicefieldGroupController.getViewValue().indexOf(attrs.value) > -1);
+        }
+        if (attrs.uifType == 'radio') {
+          checked = (choicefieldGroupController.getViewValue() === attrs.value);
+        }
         instanceElement.find('input').prop('checked', checked);
       };
       choicefieldGroupController.addRender(render);
@@ -165,7 +178,27 @@ export class ChoicefieldOptionDirective implements angular.IDirective {
         }
         scope.$apply(() => {
           if (choicefieldGroupController != null) {
-            choicefieldGroupController.setViewValue(attrs.value, ev);
+            if (attrs.uifType == 'checkbox' && ev.target.tagName.toLowerCase() == 'input') {
+              let checked: boolean = instanceElement.find('input').prop('checked');
+              let newViewValue: any = choicefieldGroupController.getViewValue();              
+              if (newViewValue && newViewValue.constructor === Array) {
+                if (checked) {
+                  newViewValue.push(attrs.value);
+                }
+                else {
+                  newViewValue.splice(newViewValue.indexOf(attrs.value), 1);
+                }
+              }
+              else {
+                if (checked){
+                  newViewValue = !!newViewValue ? [newViewValue, attrs.value] : [attrs.value];
+                }
+              }
+              choicefieldGroupController.setViewValue(newViewValue, ev);
+            }
+            if (attrs.uifType == 'radio') {
+              choicefieldGroupController.setViewValue(attrs.value, ev);
+            }
           }
         });
       });
@@ -230,7 +263,7 @@ export class ChoicefieldGroupController {
     this.renderFns.splice(this.renderFns.indexOf(fn));
   }
 
-  public setViewValue(value: string, eventType: any): void {
+  public setViewValue(value: any, eventType: any): void {
     if (typeof this.$scope.ngModel !== 'undefined' && this.$scope.ngModel != null) {
       if (this.getViewValue() !== value) {
         this.$scope.ngModel.$setDirty();
@@ -246,7 +279,7 @@ export class ChoicefieldGroupController {
     }
   }
 
-  public getViewValue(): string {
+  public getViewValue(): any {
     if (typeof this.$scope.ngModel !== 'undefined' && this.$scope.ngModel != null) {
       return this.$scope.ngModel.$viewValue;
     }
